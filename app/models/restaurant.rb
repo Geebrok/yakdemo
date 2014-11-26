fb9a31d55766fc35b5ab16282c96d855e0015eea
Grant Brooks <grantbrooks75@gmail.com>
2014-11-27 00:35:15 +1100

    add data validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,12 @@
 class Restaurant < ActiveRecord::Base
 	mount_uploader :image, ImageUploader
 	has_many :reviews
+
+	validates :name, :address, :phone, :website, :image, presence: true
+	validates :phone, format: { with: /\A\(\d{2}\) \d{4} \d{4}\z/,
+    message: "phone number must be in the following format (12) 1234 5678" }
+  validates :website, format: { with: /\Ahttps?:\/\/.*\z/,
+  	message: "must start with http:// or https://" }
+	validates :address, format: { with: /\A\d+[^,]+,[^,]+, [A-Z]{3} \d{4}\z/,
+  	message: "must be in the format 123 Fake Street, Melbourne, VIC, 3000" }
 end
