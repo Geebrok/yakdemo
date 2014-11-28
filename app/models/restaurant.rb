2e2aa473ac135969d2405dc0dff773ed8ddbe5df
Grant Brooks <grantbrooks75@gmail.com>
2014-11-28 20:54:02 +1100

    add correct grammer to qty reviews on restaurant page

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -7,6 +7,6 @@ class Restaurant < ActiveRecord::Base
     message: "phone number must be in the following format (12) 1234 5678" }
   validates :website, format: { with: /\Ahttps?:\/\/.*\z/,
   	message: "must start with http:// or https://" }
-	validates :address, format: { with: /\A\d+[^,]+,[^,]+, [A-Z]{3} \d{4}\z/,
-  	message: "must be in the format 123 Fake Street, Melbourne, VIC, 3000" }
+	validates :address, format: { with: /\A\d+[^,]+ [A-Z]+ \d{4}\z/,
+  	message: "must be in the format 123 Fake Street Melbourne VIC 3000" }
 end
